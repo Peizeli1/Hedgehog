985ed79a94cf7a667cb18e625ce384491ec0ad20
Ming-Luoyuan <liuziming464@gmail.com>
2024-12-13 05:25:49 +0000

    remove other language in settings.py

--- insert_team_name_here/code_tutors/settings.py
+++ insert_team_name_here/code_tutors/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'code_tutors.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],  # 添加模板目录路径
+        'DIRS': [BASE_DIR / 'templates'],  
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
